4e29faab3400e405fb93978c1bb962a146854234
Adheesh Shenoy <adheeshenoy@gmail.com>
2020-02-20 01:48:35 -0500

    Added marker getDistance function.

--- scripts/arPuzzle.js
+++ scripts/arPuzzle.js
@@ -1,4 +1,12 @@
 // defines the click behavior for the buttons
+var marker11;
+var marker12;
+var marker13;
+var marker14;
+var marker21;
+var marker22;
+var marker23;
+var marker24;
 var startBtn = document.getElementById("start");
 startBtn.addEventListener("click", function() {
 	btnClicked("start");
@@ -17,11 +25,6 @@ var timer = document.getElementById("timerDisplay"); // gets the timer from the 
 // Define UI behaviors
 const submitUI = document.getElementById("input");
 submitUI.style.display = "none";
-
-const iframe = document.getElementById("ARframe");
-console.log(iframe);
-const innerDoc = iframe.contentDocument || iframe.contentWindow.document;
-console.log(innerDoc);
 
 class Stopwatch {
 	constructor() {
@@ -66,6 +69,14 @@ class Stopwatch {
 		return this.timeStr();
 	}
 
+	getMillTime() {
+		var returnTime = 0;
+		returnTime += this.millis;
+		returnTime += this.secs * 1000;
+		returnTime += this.mins * 60000;
+		return returnTime;
+	}
+
 	// functions for starting, pausing, and checking whether the stopwatch is running
 	pause() {
 		this.running = false;
@@ -88,7 +99,94 @@ class arGame {
 	constuctor() {
 		this.gameOver = true;
 		this.completed = false;
-		this.scores = [];
+		this.scores = [[], [], []]; // names is scores[0], times is scores[1]
+		this.minDist = 2.5;
+	}
+
+	getDistances() {
+		var distance = new Array(10);
+		console.log(
+			Math.round(
+				marker11
+					.getAttribute("position")
+					.distanceTo(marker12.getAttribute("position")) * 100
+			) / 100
+		);
+		console.log("qwertyui");
+		distance.push(
+			Math.round(
+				marker11
+					.getAttribute("position")
+					.distanceTo(marker12.getAttribute("position")) * 100
+			) / 100
+		);
+		distance.push(
+			Math.round(
+				marker12
+					.getAttribute("position")
+					.distanceTo(marker13.getAttribute("position")) * 100
+			) / 100
+		);
+		distance.push(
+			Math.round(
+				marker13
+					.getAttribute("position")
+					.distanceTo(marker14.getAttribute("position")) * 100
+			) / 100
+		);
+		distance.push(
+			Math.round(
+				marker21
+					.getAttribute("position")
+					.distanceTo(marker22.getAttribute("position")) * 100
+			) / 100
+		);
+		distance.push(
+			Math.round(
+				marker22
+					.getAttribute("position")
+					.distanceTo(marker23.getAttribute("position")) * 100
+			) / 100
+		);
+		distance.push(
+			Math.round(
+				marker23
+					.getAttribute("position")
+					.distanceTo(marker24.getAttribute("position")) * 100
+			) / 100
+		);
+		distance.push(
+			Math.round(
+				marker11
+					.getAttribute("position")
+					.distanceTo(marker21.getAttribute("position")) * 100
+			) / 100
+		);
+		distance.push(
+			Math.round(
+				marker12
+					.getAttribute("position")
+					.distanceTo(marker22.getAttribute("position")) * 100
+			) / 100
+		);
+		distance.push(
+			Math.round(
+				marker13
+					.getAttribute("position")
+					.distanceTo(marker23.getAttribute("position")) * 100
+			) / 100
+		);
+		distance.push(
+			Math.round(
+				marker14
+					.getAttribute("position")
+					.distanceTo(marker24.getAttribute("position")) * 100
+			) / 100
+		);
+		for (let i = 0; i < distance.length; i++) {
+			console.log(distance[i]);
+		}
+		return distance;
 	}
 
 	// says whether the puzzle is completed
@@ -108,10 +206,11 @@ class arGame {
 
 	// takes in a list of distances and a minimum distance. If any of the distances are greater than the minimum
 	// distance, the game is not completed.
-	checkCompleted(newDists, minDist) {
+	checkCompleted() {
+		var newDists = this.getDistances();
 		this.completed = true;
-		for (dist in newDists)
-			if (dist > minDist) {
+		for (let dist of newDists)
+			if (dist >= this.minDist || dist <= 0.000001) {
 				this.completed = false;
 				break;
 			}
@@ -119,17 +218,15 @@ class arGame {
 		return this.completed;
 	}
 
-	// inserts a new score into the list of scores
-	addScore(name, timeStr, totalMillis) {}
-
-	// progresses the game state and calculates game information from the current state
-	continue() {}
+	// trys to add a new score into the list of scores
+	// if the new score is in the top 5, it will be added
+	tryToAddScore(name, timeStr, totalMillis) {}
 }
 
 // function that progresses the game
 var gameLoop = function() {
 	if (watch.isRunning()) timer.innerHTML = watch.tick(100); // updates the time in the UI
-	if (game.checkCompleted(getDistances, 2.5))
+	if (game.checkCompleted())
 		// gets the current distances and checks if the puzzle is completed.
 		endGame(); // ends the game if the puzzle is completed
 };
@@ -140,9 +237,17 @@ function startGame() {
 	startBtn.style.display = "none";
 	pauseResetBtn.style.display = "inline";
 	pauseResetBtn.innerHTML = "Pause";
-
+	var iframe = document.getElementById("ARframe").contentWindow.document;
 	// starts the watch
 	watch.start();
+	marker11 = iframe.getElementById("marker1");
+	marker12 = iframe.getElementById("marker2");
+	marker13 = iframe.getElementById("marker3");
+	marker14 = iframe.getElementById("marker4");
+	marker21 = iframe.getElementById("marker5");
+	marker22 = iframe.getElementById("marker6");
+	marker23 = iframe.getElementById("marker7");
+	marker24 = iframe.getElementById("marker8");
 
 	// !!TODO!! use clearInterval(gameInterval) somewhere to end the game loop
 	gameInterval = setInterval(gameLoop, 100);
@@ -163,10 +268,23 @@ function resetGame() {
 	pauseResetBtn.style.display = "none";
 }
 
+//Game is completed
 function endGame() {
 	clearInterval(gameInterval); // stops the game loop
 	watch.pause();
 	displayEndScreen();
+}
+
+function processEnd() {
+	// var newTimeStr = Take from HTML
+	var newMilli = watch.getMillTime();
+	// var name = Name submitted
+	// game.tryToAddScore(name, newTimeStr, newMilli);
+	// writeScoresToFile(game.getScores());
+
+	// TODO: replace this with
+
+	resetGame();
 }
 
 var btnClicked = function(btnName) {
@@ -179,6 +297,7 @@ var btnClicked = function(btnName) {
 			else resetGame();
 			break;
 		case "submitBtn":
+			processEnd();
 			break;
 		default:
 			console.log("DEFAULT STATEMENT REACHED");
@@ -193,6 +312,7 @@ var game = new arGame();
 function displayEndScreen() {
 	const gameSection = document.getElementById("game");
 	const ARFrame = document.getElementById("ARframe");
+	pauseResetBtn.innerHTML = "Reset";
 	gameSection.className = "game-end";
 	ARFrame.className = "frame-end";
 	submitUI.style.display = "inline";
